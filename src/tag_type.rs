da6dca2f92c14904172456e2aa50b3e2524cbbcb
ThatCoolCoder <ThatCoolCoder@users.noreply.github.com>
2022-08-13 08:40:12 +0930

    Continue with new tokeniser

--- src/tag_type.rs
+++ src/tag_type.rs
@@ -1,5 +1,6 @@
+#[derive(PartialEq)]
 pub enum TagType {
-    Start, // like <p>
-    End, // like </p>
-    Standalone // like <input />
+    Start,      // like <p>
+    End,        // like </p>
+    Standalone, // like <input />
 }
--- src/tokeniser_new.rs
+++ src/tokeniser_new.rs
@@ -11,7 +11,7 @@ use std::collections::HashMap;
 pub enum Token {
     Tag(TagToken),
     Content(ContentToken),
-    Javascript(JavascriptToken),
+    InlineJavascript(InlineJavascriptToken),
 }
 
 // Represents a single html tag - <opening>, </closing> or <standalone />
@@ -25,7 +25,7 @@ pub struct ContentToken {
     pub value: String,
 }
 // Represents a chunk of javascript found in the markup
-pub struct JavascriptToken {
+pub struct InlineJavascriptToken {
     pub value: String,
     pub javascript_type: JavascriptType,
 }
@@ -38,13 +38,69 @@ pub struct JavascriptToken {
 //     Curly,
 // }
 
-fn read_html_tag() {}
+fn read_markup(markup: &str) -> Vec<Token> {
+    let mut remaining = markup.to_string();
+    let mut inside_script_tag = false;
+    let mut result = vec![];
+    while remaining.len() > 0 {
+        // Read tag
+        if remaining.chars().next().unwrap() == '<' {
+            let (tag, chars) = read_html_tag(&remaining);
+            remaining.drain(chars..);
+            inside_script_tag = tag.name == "<script>" && tag.tag_type == TagType::Start;
+            result.push(Token::Tag(tag));
+        }
+        // Read inline javascript
+        else if remaining.chars().next().unwrap() == '~' {
+            let inline_js = read_inline_javascript(&remaining);
+            remaining.drain(..inline_js.len());
+            result.push(Token::InlineJavascript(InlineJavascriptToken {
+                value: inline_js,
+                javascript_type: find_javascript_type(inline_js),
+            }))
+        }
+        // Read script tag content
+        else if inside_script_tag {
+            let js = read_javascript(markup);
+            remaining.drain(..js.len());
+        }
+        // Read normal tag content
+        else {
+            let content = read_tag_content(markup);
+            remaining.drain(..content.len());
+        }
+    }
+    return result;
+}
 
-fn read_tag_content() {}
+fn read_html_tag(markup: &str) -> (TagToken, usize) {
+    // Read an open/close/standalone tag. Second return value is tag length
+
+    return (
+        TagToken {
+            name: "".to_string(),
+            attributes: vec![],
+            tag_type: TagType::Start,
+        },
+        5,
+    );
+}
+
+fn read_tag_content(markup: &str) -> String {
+    // Read the content (inner text) of a tag.
+    let mut result = "".to_string();
+    for char in markup.chars() {
+        if char == '<' {
+            break;
+        }
+        result.push(char);
+    }
+    return result;
+}
 
 fn read_javascript(data: &str) -> String {
-    // Read some JavaScript until it is ended by a <script> tag.
-    // Does not return the script tag
+    // Read some JavaScript until it is ended by a </script> tag.
+    // Returned value does not include the ending script tag
 
     // todo: when I have internet get a library to make this a const
     let brackets: HashMap<char, bool> = [
@@ -84,8 +140,32 @@ fn read_javascript(data: &str) -> String {
             result.push(char);
             char_idx += 1;
         }
+        if bracket_stack.len() == 0 && &data[char_idx..] == "</script>" {
+            break;
+        }
     }
     return result;
+}
+
+fn read_inline_javascript(markup: &str) -> (InlineJavascriptToken, usize) {
+    // Read inline javascript.
+    // Expects markup to begin with a tilde (inline start char)
+
+    let mut result = "".to_string();
+    for char in markup.chars().skip(1) {
+        result.push(char);
+        if char == '~' || char == '\n' {
+            break;
+        }
+    }
+    let javascript_type = find_javascript_type(&result);
+    return (
+        InlineJavascriptToken {
+            value: result,
+            javascript_type: javascript_type,
+        },
+        result.len(),
+    );
 }
 
 fn read_string(quote_char: char, escape_char: char, data: &str) -> String {
@@ -111,4 +191,31 @@ fn read_string(quote_char: char, escape_char: char, data: &str) -> String {
         }
     }
     return result;
+}
+
+fn find_tag_type(tag_content: &str) -> TagType {
+    // Tag content should not include angle brackets
+
+    // </tag>
+    if tag_content.chars().skip(1).next().unwrap() == '/' {
+        return TagType::End;
+    }
+    // <tag />
+    else if tag_content.chars().last().unwrap() == '/' {
+        return TagType::Standalone;
+    }
+    // <tag>
+    else {
+        return TagType::Start;
+    }
+}
+
+fn find_javascript_type(javascript: &str) -> JavascriptType {
+    if javascript.chars().last().unwrap() == '{' {
+        return JavascriptType::BlockStart;
+    } else if javascript.chars().next().unwrap() == '}' {
+        return JavascriptType::BlockEnd;
+    } else {
+        return JavascriptType::Standalone;
+    }
 }
