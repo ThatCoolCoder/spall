8b3820c07f8cfe222a0e6f8abea2dedebca701df
ThatCoolCoder <ThatCoolCoder@users.noreply.github.com>
2022-08-28 20:15:46 +0930

    Import system for the framework files , because why not

--- runtime/SpallPage.js
+++ runtime/SpallPage.js
@@ -1,8 +1,8 @@
 requires(SpallElement.js);
 
-// class SpallPage extends SpallElement {
-//     constructor(title, elementName, id, parentId, renderer, path) {
-//         super(elementName, id, parentId, renderer, path);
-//         this.title = title;
-//     }
-// }
+class SpallPage extends SpallElement {
+    constructor(title, elementName, id, parentId, renderer, path) {
+        super(elementName, id, parentId, renderer, path);
+        this.title = title;
+    }
+}
